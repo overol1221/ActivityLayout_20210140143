8ae92e924a4b55477fd0d3f5b023c7ff8d5ef3e0
overol1221 <fiqiachmad12@gmail.com>
2023-10-05 01:46:33 +0700

    FINISH

---
+++ app/src/main/java/com/example/pertemuan3/LinearLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
